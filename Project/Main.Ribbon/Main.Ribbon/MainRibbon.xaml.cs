8f2116e1df90365c418f5e9a06abde934789f77c
Sadness96 <qsbs623@163.com>
2017-09-28 09:41:59 +0800

    Modify:Ribbon Code Generation

--- Project/Main.Ribbon/Main.Ribbon/MainRibbon.xaml.cs
+++ Project/Main.Ribbon/Main.Ribbon/MainRibbon.xaml.cs
@@ -11,7 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using DevExpress.Utils.About;
 using DevExpress.Xpf.Ribbon;
+using DevExpress.Xpf.Bars;
 
 namespace Main.Ribbon
 {
@@ -26,16 +28,45 @@ namespace Main.Ribbon
         public MainRibbon()
         {
             InitializeComponent();
+            if (base.Height > SystemParameters.VirtualScreenHeight || base.Width > SystemParameters.VirtualScreenWidth)
+            {
+                base.WindowState = WindowState.Maximized;
+            }
+            UAlgo.Default.DoEventObject(1, 3, this);
         }
 
         /// <summary>
-        /// 菜单下分组按钮
+        /// MainRibbon 的Load方法
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void groupFile_CaptionButtonClick(object sender, RibbonCaptionButtonClickEventArgs e)
+        private void MainRibbon_Loaded(object sender, RoutedEventArgs e)
         {
+            //修改基础样式(默认RibbonStyle.Office2007)
+            RibbonControl.RibbonStyle = RibbonStyle.Office2010;
+            //设置软件图标
+            //RibbonControl.ApplicationButtonSmallIcon = new BitmapImage(new Uri(@"F:\IC#\Ribbon_Code_Demo\Ribbon_Code_Demo\Image\10n.ico", UriKind.Absolute));
+            //RibbonControl.ApplicationButtonLargeIcon = new BitmapImage(new Uri(@"F:\IC#\Ribbon_Code_Demo\Ribbon_Code_Demo\Image\10n.ico", UriKind.Absolute));
+            //设置对齐方式
+            RibbonControl.ApplicationButtonText = "File";
+            RibbonControl.PageCategoryAlignment = RibbonPageCategoryCaptionAlignment.Right;
+            //创建RibbonPage
+            RibbonPage ribbonPageHome = new RibbonPage();
+            ribbonPageHome.Caption = "Home";
+            //创建RibbonPageGroup
+            RibbonPageGroup ribbonPageGroup = new RibbonPageGroup();
+            ribbonPageGroup.Caption = "File";
+            //创建BarButtonItem
+            BarButtonItem barButton = new BarButtonItem();
+            barButton.RibbonStyle = RibbonItemStyles.Large;
+            barButton.Content = "New";
+            //barButton.LargeGlyph = new BitmapImage(new Uri(@"F:\IC#\Ribbon_Code_Demo\Ribbon_Code_Demo\Image\10n.ico", UriKind.Absolute));
 
+            ribbonPageGroup.ItemLinks.Add(barButton);
+
+            ribbonPageHome.Groups.Add(ribbonPageGroup);
+
+            PageCategory.Pages.Add(ribbonPageHome);
         }
     }
 }
