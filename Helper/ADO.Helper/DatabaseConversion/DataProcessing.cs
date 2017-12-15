3817d400be6b00e1f879df0c44044da49cbeb9e4
Sadness96 <qsbs623@163.com>
2017-12-15 11:59:32 +0800

    Modify:Convert To List<T>

--- Helper/ADO.Helper/DatabaseConversion/DataProcessing.cs
+++ Helper/ADO.Helper/DatabaseConversion/DataProcessing.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Data;
+using System.Threading.Tasks;
+using System.Reflection;
 using ADO.Helper.TXT;
 
 namespace ADO.Helper.DatabaseConversion
@@ -71,6 +73,85 @@ namespace ADO.Helper.DatabaseConversion
                 TXTHelper.Logs(ex.ToString());
             }
             return dtDataSource;
+        }
+
+        /// <summary>
+        /// DataTable转换为List<T>
+        /// </summary>
+        /// <typeparam name="T">数据模型</typeparam>
+        /// <param name="dtDataSource">源数据(DataTable)</param>
+        /// <returns>成功返回List<T>,失败返回null</returns>
+        public static List<T> ConvertToList<T>(DataTable dtDataSource) where T : class,new()
+        {
+            try
+            {
+                List<T> listT = new List<T>();
+                foreach (DataRow drDataSource in dtDataSource.Rows)
+                {
+                    T t = new T();
+                    PropertyInfo[] propertyInfos = t.GetType().GetProperties();
+                    foreach (PropertyInfo propertyInfo in propertyInfos)
+                    {
+                        string tempName = propertyInfo.Name;
+                        if (dtDataSource.Columns.Contains(tempName))
+                        {
+                            if (!propertyInfo.CanWrite) continue;
+                            object value = drDataSource[tempName];
+                            if (value != DBNull.Value)
+                            {
+                                if (propertyInfo.GetMethod.ReturnParameter.ParameterType.Name == "Int32")
+                                {
+                                    value = Convert.ToInt32(value);
+                                }
+                                propertyInfo.SetValue(t, value, null);
+                            }
+                        }
+                    }
+                    listT.Add(t);
+                }
+                return listT;
+            }
+            catch (Exception ex)
+            {
+                TXTHelper.Logs(ex.ToString());
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// List<T>转换为DataTable
+        /// </summary>
+        /// <param name="listDataSource">源数据</param>
+        /// <returns>成功返回DataTable,失败返回null</returns>
+        public static DataTable ConvertDataTable(IList listDataSource)
+        {
+            try
+            {
+                DataTable dataTable = new DataTable();
+                if (listDataSource.Count > 0)
+                {
+                    PropertyInfo[] propertyInfos = listDataSource[0].GetType().GetProperties();
+                    foreach (PropertyInfo propertyInfo in propertyInfos)
+                    {
+                        dataTable.Columns.Add(propertyInfo.Name, propertyInfo.PropertyType);
+                    }
+                    foreach (var vDataSource in listDataSource)
+                    {
+                        ArrayList arrayList = new ArrayList();
+                        foreach (PropertyInfo propertyInfo in propertyInfos)
+                        {
+                            arrayList.Add(propertyInfo.GetValue(vDataSource, null));
+                        }
+                        dataTable.LoadDataRow(arrayList.ToArray(), true);
+                    }
+                }
+                return dataTable;
+            }
+            catch (Exception ex)
+            {
+                TXTHelper.Logs(ex.ToString());
+            }
+            return null;
         }
     }
 }
