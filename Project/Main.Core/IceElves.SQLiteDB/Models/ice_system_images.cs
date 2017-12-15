fb685c5ca8d8688d857b57e1a5c1bf8cbca01ee8
Sadness96 <qsbs623@163.com>
2017-12-15 12:04:08 +0800

    Modify:SQLite ADO Connect

---
+++ Project/Main.Core/IceElves.SQLiteDB/Models/ice_system_images.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceElves.SQLiteDB.Models
+{
+    /// <summary>
+    /// ice_system_images 表数据模型
+    /// </summary>
+    public partial class ice_system_images
+    {
+        /// <summary>
+        /// 主键ID
+        /// </summary>
+        public int ice_id { get; set; }
+        /// <summary>
+        /// Image_Key
+        /// </summary>
+        public string ice_sys_key { get; set; }
+        /// <summary>
+        /// Image_Value
+        /// </summary>
+        public byte[] ice_sys_value { get; set; }
+    }
+}
--- Project/Main.Window/Main.Console/Program.cs
+++ Project/Main.Window/Main.Console/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 using System.Threading.Tasks;
 
 namespace Main.Console
@@ -10,6 +11,7 @@ namespace Main.Console
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
