9e5deecd4866b0094c8c1c2cf36df599d0367f96
Sadness96 <qsbs623@163.com>
2018-01-10 15:04:58 +0800

    Add:Aes Encryption Helper

---
+++ Helper/Utils.Helper/Encryption/AESHelper.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.Helper.TXT;
+
+namespace Utils.Helper.Encryption
+{
+    /// <summary>
+    /// AES加密解密帮助类
+    /// 创建日期:2018年1月10日
+    /// </summary>
+    public class AESHelper
+    {
+        /// <summary>
+        /// AES加密
+        /// </summary>
+        /// <param name="strPlaintext">明文</param>
+        /// <param name="strKey">秘钥</param>
+        /// <returns>AES密文</returns>
+        public static string AESEncrypt(string strPlaintext, string strKey)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(strPlaintext))
+                {
+                    return string.Empty;
+                }
+                strKey = strKey.Length < 32 ? strKey.PadRight(32, '0') : strKey.Substring(0, 32);
+                Byte[] toEncryptArray = Encoding.UTF8.GetBytes(strPlaintext);
+                RijndaelManaged rijndaelManaged = new RijndaelManaged
+                {
+                    Key = Encoding.UTF8.GetBytes(strKey),
+                    Mode = CipherMode.ECB,
+                    Padding = PaddingMode.PKCS7
+                };
+                ICryptoTransform pCryptoTransform = rijndaelManaged.CreateEncryptor();
+                Byte[] resultArray = pCryptoTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+                return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+            }
+            catch (Exception ex)
+            {
+                TXTHelper.Logs(ex.ToString());
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// AES解密
+        /// </summary>
+        /// <param name="strCiphertext">AES密文</param>
+        /// <param name="strKey">秘钥</param>
+        /// <returns>明文</returns>
+        public static string AESDecrypt(string strCiphertext, string strKey)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(strCiphertext))
+                {
+                    return string.Empty;
+                }
+                strKey = strKey.Length < 32 ? strKey.PadRight(32, '0') : strKey.Substring(0, 32);
+                Byte[] toEncryptArray = Convert.FromBase64String(strCiphertext);
+                RijndaelManaged rijndaelManaged = new RijndaelManaged
+                {
+                    Key = Encoding.UTF8.GetBytes(strKey),
+                    Mode = CipherMode.ECB,
+                    Padding = PaddingMode.PKCS7
+                };
+                ICryptoTransform pCryptoTransform = rijndaelManaged.CreateDecryptor();
+                Byte[] resultArray = pCryptoTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+                return Encoding.UTF8.GetString(resultArray);
+            }
+            catch (Exception ex)
+            {
+                TXTHelper.Logs(ex.ToString());
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// 文件AES加密
+        /// </summary>
+        /// <param name="strFilePath">文件路径</param>
+        /// <param name="strSaveFilePath">加密文件目录</param>
+        /// <param name="strKey">秘钥</param>
+        /// <returns>成功返回true,失败返回false</returns>
+        public static bool FileAESEncrypt(string strFilePath, string strSaveFilePath, string strKey)
+        {
+            try
+            {
+                //设置Aes秘钥和格式
+                strKey = strKey.Length < 32 ? strKey.PadRight(32, '0') : strKey.Substring(0, 32);
+                RijndaelManaged rijndaelManaged = new RijndaelManaged
+                {
+                    Key = Encoding.UTF8.GetBytes(strKey),
+                    Mode = CipherMode.ECB,
+                    Padding = PaddingMode.PKCS7
+                };
+                //读取文本加密数据
+                FileStream fileStream = File.OpenRead(strFilePath);
+                byte[] byteFileStream = new byte[fileStream.Length];
+                fileStream.Read(byteFileStream, 0, (int)fileStream.Length);
+                fileStream.Close();
+                using (var memoryStream = new MemoryStream())
+                {
+                    using (var cryptoStream = new CryptoStream(memoryStream, rijndaelManaged.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(byteFileStream, 0, byteFileStream.Length);
+                        cryptoStream.FlushFinalBlock();
+                        fileStream = File.OpenWrite(strSaveFilePath);
+                        foreach (byte byteMemoryStream in memoryStream.ToArray())
+                        {
+                            fileStream.WriteByte(byteMemoryStream);
+                        }
+                        fileStream.Close();
+                        cryptoStream.Close();
+                        memoryStream.Close();
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                TXTHelper.Logs(ex.ToString());
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 文件AES解密
+        /// </summary>
+        /// <param name="strFilePath">被加密的文件路径</param>
+        /// <param name="strSaveFilePath">解密文件目录</param>
+        /// <param name="strKey">秘钥</param>
+        /// <returns>成功返回true,失败返回false</returns>
+        public static bool FileAESDecrypt(string strFilePath, string strSaveFilePath, string strKey)
+        {
+            try
+            {
+                strKey = strKey.Length < 32 ? strKey.PadRight(32, '0') : strKey.Substring(0, 32);
+                RijndaelManaged rijndaelManaged = new RijndaelManaged
+                {
+                    Key = Encoding.UTF8.GetBytes(strKey),
+                    Mode = CipherMode.ECB,
+                    Padding = PaddingMode.PKCS7
+                };
+                FileStream fileStream = File.OpenRead(strFilePath);
+                byte[] byteFileStream = new byte[fileStream.Length];
+                fileStream.Read(byteFileStream, 0, (int)fileStream.Length);
+                fileStream.Close();
+                using (var memoryStream = new MemoryStream())
+                {
+                    using (var cryptoStream = new CryptoStream(memoryStream, rijndaelManaged.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(byteFileStream, 0, byteFileStream.Length);
+                        cryptoStream.FlushFinalBlock();
+                        fileStream = File.OpenWrite(strSaveFilePath);
+                        foreach (byte byteMemoryStream in memoryStream.ToArray())
+                        {
+                            fileStream.WriteByte(byteMemoryStream);
+                        }
+                        fileStream.Close();
+                        cryptoStream.Close();
+                        memoryStream.Close();
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                TXTHelper.Logs(ex.ToString());
+                return false;
+            }
+        }
+    }
+}
--- Project/Main.Window/Main.Console/Program.cs
+++ Project/Main.Window/Main.Console/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Data;
 using System.Threading.Tasks;
-using FileIO.Helper.ZIP;
+using Utils.Helper.Encryption;
 using IceElves.SQLiteDB.Connect;
 using IceElves.SQLiteDB.Utils;
 using IceElves.SQLiteDB.Models;
@@ -15,19 +15,9 @@ namespace Main.Console
     {
         static void Main(string[] args)
         {
-            //List<string> list = new List<string>();
-            //list.Add(@"E:\读取文件路径测试\1\2\3\4.txt");
-            //list.Add(@"E:\读取文件路径测试\1.txt");
-            //ZIP7Helper.Compression7Zip(@"C:\Users\XiaoHua\Desktop\zipTest\test1.7z", list.ToArray());
-            //ZIP7Helper.Compression7Zip(@"C:\Users\XiaoHua\Desktop\zipTest\test2.7z", "123", list.ToArray());
-
-            //ZIP7Helper.Compression7ZipDirectory(@"C:\Users\XiaoHua\Desktop\zipTest\test11.7z", @"E:\读取文件路径测试");
-            //ZIP7Helper.Compression7ZipDirectory(@"C:\Users\XiaoHua\Desktop\zipTest\test22.7z", "123", @"E:\读取文件路径测试");
-
-            ZIP7Helper.DeCompression7Zip(@"C:\Users\XiaoHua\Desktop\zipTest\test1.7z", @"C:\Users\XiaoHua\Desktop\zipTest\test1");
-            ZIP7Helper.DeCompression7Zip(@"C:\Users\XiaoHua\Desktop\zipTest\test2.7z", @"C:\Users\XiaoHua\Desktop\zipTest\test2", "123");
-            ZIP7Helper.DeCompression7Zip(@"C:\Users\XiaoHua\Desktop\zipTest\test11.7z", @"C:\Users\XiaoHua\Desktop\zipTest\test11");
-            ZIP7Helper.DeCompression7Zip(@"C:\Users\XiaoHua\Desktop\zipTest\test22.7z", @"C:\Users\XiaoHua\Desktop\zipTest\test22", "123");
+            bool b1 = AESHelper.FileAESEncrypt(@"C:\Users\XiaoHua\Desktop\Navicat_PDF_Win.pdf", @"C:\Users\XiaoHua\Desktop\Navicat_PDF_Win(1).pdf", "123321");
+            bool b2 = AESHelper.FileAESDecrypt(@"C:\Users\XiaoHua\Desktop\Navicat_PDF_Win(1).pdf", @"C:\Users\XiaoHua\Desktop\Navicat_PDF_Win(2).pdf", "123321");
+            bool b3 = AESHelper.FileAESDecrypt(@"C:\Users\XiaoHua\Desktop\Navicat_PDF_Win(1).pdf", @"C:\Users\XiaoHua\Desktop\Navicat_PDF_Win(3).pdf", "124321");
         }
     }
 }
