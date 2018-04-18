1522ef85d0f77328e53655d59f9d17f64c7fdbaf
Sadness96 <qsbs623@163.com>
2018-04-18 17:53:10 +0800

    Add:Image Helper,Registration tool

---
+++ Code/Helper/Utils.Helper/Image/ImageHelper.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Utils.Helper.Image
+{
+    /// <summary>
+    /// 图片帮助类
+    /// </summary>
+    public class ImageHelper
+    {
+        /// <summary>
+        /// 转化图片二进制流到BitmapImage
+        /// </summary>
+        /// <param name="byteArray">图片二进制流</param>
+        /// <returns>成功返回BitmapImage,失败返回null</returns>
+        public static BitmapImage ByteArrayToBitMapImage(byte[] byteArray)
+        {
+            try
+            {
+                using (var memoryStream = new System.IO.MemoryStream(byteArray))
+                {
+                    BitmapImage bitmapImage = new BitmapImage();
+                    bitmapImage.BeginInit();
+                    bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapImage.StreamSource = memoryStream;
+                    bitmapImage.EndInit();
+                    bitmapImage.Freeze();
+                    return bitmapImage;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 转化图片二进制流到ImageSource
+        /// </summary>
+        /// <param name="byteArray">图片二进制流</param>
+        /// <returns>成功返回ImageSource,失败返回null</returns>
+        public static ImageSource ByteArrayToImageSource(byte[] byteArray)
+        {
+            try
+            {
+                Bitmap bitmap = ByteArrayToBitmap(byteArray);
+                IntPtr hBitmap = bitmap.GetHbitmap();
+                ImageSource imageSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hBitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                return imageSource;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 转化图片二进制流到Bitmap
+        /// </summary>
+        /// <param name="byteArray">图片二进制流</param>
+        /// <returns>成功返回Bitmap,失败返回null</returns>
+        public static Bitmap ByteArrayToBitmap(byte[] byteArray)
+        {
+            MemoryStream memoryStream = null;
+            try
+            {
+                memoryStream = new MemoryStream(byteArray);
+                return new Bitmap((System.Drawing.Image)new Bitmap(memoryStream));
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                memoryStream.Close();
+            }
+        }
+    }
+}
--- Code/Project/Main.Function/Sadness.BasicFunction/ViewModels/PluginMenu/RegistrationToolViewModel.cs
+++ Code/Project/Main.Function/Sadness.BasicFunction/ViewModels/PluginMenu/RegistrationToolViewModel.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
+using Sadness.SQLiteDB.Connect;
+using Utils.Helper.Image;
+using Utils.Helper.PCInformation;
 
 namespace Sadness.BasicFunction.ViewModels.PluginMenu
 {
@@ -19,14 +22,14 @@ namespace Sadness.BasicFunction.ViewModels.PluginMenu
         {
             //应用程序标题
             Title = "注册工具";
-            //应用程序背景
-            Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#505050"));
+            //设置软件图标
+            MainAppLargeIcon = ImageHelper.ByteArrayToImageSource(MainImage.GetImageByteArray("AppLargeIcon"));
         }
 
         /// <summary>
         /// 应用程序标题
         /// </summary>
-        private string _Title;
+        private string _title;
         /// <summary>
         /// 应用程序标题
         /// </summary>
@@ -34,30 +37,137 @@ namespace Sadness.BasicFunction.ViewModels.PluginMenu
         {
             get
             {
-                return _Title;
+                return _title;
             }
             set
             {
-                _Title = value;
+                _title = value;
             }
         }
 
         /// <summary>
-        /// 应用程序背景
+        /// 应用程序大图标
         /// </summary>
-        private Brush _background;
+        private ImageSource _mainAppLargeIcon;
         /// <summary>
-        /// 应用程序背景
+        /// 应用程序大图标
         /// </summary>
-        public Brush Background
+        public ImageSource MainAppLargeIcon
         {
             get
             {
-                return _background;
+                return _mainAppLargeIcon;
             }
             set
             {
-                _background = value;
+                _mainAppLargeIcon = value;
+            }
+        }
+
+        /// <summary>
+        /// 读取网卡MAC地址
+        /// </summary>
+        public List<string> ListMAC
+        {
+            get
+            {
+                return PCInformationHelper.MAC();
+            }
+        }
+
+        /// <summary>
+        /// 读取CPU-ID
+        /// </summary>
+        public List<string> ListCPU
+        {
+            get
+            {
+                return PCInformationHelper.CPU();
+            }
+        }
+
+        /// <summary>
+        /// 读取硬盘序列号
+        /// </summary>
+        public List<string> ListDESK
+        {
+            get
+            {
+                return PCInformationHelper.DESK();
+            }
+        }
+
+        /// <summary>
+        /// 读取内存序列号
+        /// </summary>
+        public List<string> ListMemory
+        {
+            get
+            {
+                return PCInformationHelper.Memory();
+            }
+        }
+
+        /// <summary>
+        /// 读取主板序列号
+        /// </summary>
+        public List<string> ListMotherboard
+        {
+            get
+            {
+                return PCInformationHelper.Motherboard();
+            }
+        }
+
+        /// <summary>
+        /// 读取BIOS序列号
+        /// </summary>
+        public List<string> ListBIOS
+        {
+            get
+            {
+                return PCInformationHelper.BIOS();
+            }
+        }
+
+        /// <summary>
+        /// 读取显卡信息
+        /// </summary>
+        public List<string> ListVideo
+        {
+            get
+            {
+                return PCInformationHelper.Video();
+            }
+        }
+
+        /// <summary>
+        /// 注册请求码
+        /// </summary>
+        private string _requestCode { get; set; }
+        /// <summary>
+        /// 注册请求码
+        /// </summary>
+        public string RequestCode
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_requestCode))
+                {
+                    _requestCode = EncryptionHelper.RequestCode();
+                }
+                return _requestCode;
+            }
+        }
+
+        /// <summary>
+        /// 注册激活码
+        /// </summary>
+        public string ActivationCode
+        {
+            get
+            {
+                return EncryptionHelper.ActivationCode(_requestCode);
             }
         }
     }
