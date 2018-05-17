4fd5e1d38cafba5be3fe79ba9302a09e10ac4770
Sadness <qsbs623@163.com>
2018-05-17 23:30:47 +0800

    Modify:DES Encryption Tool

--- Code/Project/Main.Function/Sadness.BasicFunction/ViewModels/PluginMenu/EncryptionViewModel.cs
+++ Code/Project/Main.Function/Sadness.BasicFunction/ViewModels/PluginMenu/EncryptionViewModel.cs
@@ -133,7 +133,7 @@ namespace Sadness.BasicFunction.ViewModels.PluginMenu
         {
             get
             {
-                return new DelegateCommand(delegate()
+                return new DelegateCommand(delegate ()
                 {
                     if (string.IsNullOrEmpty(_textAesKey))
                     {
@@ -152,7 +152,7 @@ namespace Sadness.BasicFunction.ViewModels.PluginMenu
         {
             get
             {
-                return new DelegateCommand(delegate()
+                return new DelegateCommand(delegate ()
                 {
                     if (string.IsNullOrEmpty(_textAesKey))
                     {
@@ -171,7 +171,7 @@ namespace Sadness.BasicFunction.ViewModels.PluginMenu
         {
             get
             {
-                return new DelegateCommand(delegate()
+                return new DelegateCommand(delegate ()
                 {
                     if (string.IsNullOrEmpty(_textAesKey))
                     {
@@ -205,7 +205,7 @@ namespace Sadness.BasicFunction.ViewModels.PluginMenu
         {
             get
             {
-                return new DelegateCommand(delegate()
+                return new DelegateCommand(delegate ()
                 {
                     if (string.IsNullOrEmpty(_textAesKey))
                     {
@@ -234,7 +234,187 @@ namespace Sadness.BasicFunction.ViewModels.PluginMenu
         #endregion
 
         #region DES加密 Tab分页
+        /// <summary>
+        /// Des加密源
+        /// </summary>
+        private string _textDesSource;
+        /// <summary>
+        /// Des加密源
+        /// </summary>
+        public string TextDesSource
+        {
+            get
+            {
+                return _textDesSource;
+            }
+            set
+            {
+                SetProperty(ref _textDesSource, value);
+            }
+        }
 
+        /// <summary>
+        /// Des加密目标
+        /// </summary>
+        private string _textDesTarget;
+        /// <summary>
+        /// Des加密目标
+        /// </summary>
+        public string TextDesTarget
+        {
+            get
+            {
+                return _textDesTarget;
+            }
+            set
+            {
+                SetProperty(ref _textDesTarget, value);
+            }
+        }
+
+        /// <summary>
+        /// Des加密秘钥
+        /// </summary>
+        private string _textDesKey;
+        /// <summary>
+        /// Des加密秘钥
+        /// </summary>
+        public string TextDesKey
+        {
+            get
+            {
+                return _textDesKey;
+            }
+            set
+            {
+                SetProperty(ref _textDesKey, value);
+            }
+        }
+
+        /// <summary>
+        /// Des加密向量
+        /// </summary>
+        private string _textDesIV;
+        /// <summary>
+        /// Des加密向量
+        /// </summary>
+        public string TextDesIV
+        {
+            get
+            {
+                return _textDesIV;
+            }
+            set
+            {
+                SetProperty(ref _textDesIV, value);
+            }
+        }
+
+        /// <summary>
+        /// Des加密
+        /// </summary>
+        public DelegateCommand DesEncrypt
+        {
+            get
+            {
+                return new DelegateCommand(delegate ()
+                {
+                    if (string.IsNullOrEmpty(_textDesKey) || string.IsNullOrEmpty(_textDesIV))
+                    {
+                        MessageBox.Show("请输入秘钥和向量!");
+                        return;
+                    }
+                    TextDesTarget = DESHelper.DESEncrypt(TextDesSource, TextDesKey, TextDesIV);
+                });
+            }
+        }
+
+        /// <summary>
+        /// Des解密
+        /// </summary>
+        public DelegateCommand DesDecrypt
+        {
+            get
+            {
+                return new DelegateCommand(delegate ()
+                {
+                    if (string.IsNullOrEmpty(_textDesKey) || string.IsNullOrEmpty(_textDesIV))
+                    {
+                        MessageBox.Show("请输入秘钥和向量!");
+                        return;
+                    }
+                    TextDesTarget = DESHelper.DESDecrypt(TextDesSource, TextDesKey, TextDesIV);
+                });
+            }
+        }
+
+        /// <summary>
+        /// Des加密文件
+        /// </summary>
+        public DelegateCommand DesEncryptFile
+        {
+            get
+            {
+                return new DelegateCommand(delegate ()
+                {
+                    if (string.IsNullOrEmpty(_textDesKey) || string.IsNullOrEmpty(_textDesIV))
+                    {
+                        MessageBox.Show("请输入秘钥和向量!");
+                        return;
+                    }
+                    Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+                    dialog.Title = "选择文件";
+                    dialog.Filter = "所有文件|*.*";
+                    if (dialog.ShowDialog() == true)
+                    {
+                        string strFilePath = dialog.FileName;
+                        string strSaveFilePath = string.Format("{0}\\{1}(Encrypt){2}", Path.GetDirectoryName(strFilePath), Path.GetFileNameWithoutExtension(strFilePath), Path.GetExtension(strFilePath));
+                        if (DESHelper.FileDESEncrypt(strFilePath, strSaveFilePath, TextDesKey, TextDesIV))
+                        {
+                            MessageBox.Show("Des文件加密成功!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Des文件加密失败!");
+                        }
+                    }
+                });
+            }
+        }
+
+        /// <summary>
+        /// Des解密文件
+        /// </summary>
+        public DelegateCommand DesDecryptFile
+        {
+            get
+            {
+                return new DelegateCommand(delegate ()
+                {
+                    if (string.IsNullOrEmpty(_textDesKey) || string.IsNullOrEmpty(_textDesIV))
+                    {
+                        MessageBox.Show("请输入秘钥和向量!");
+                        return;
+                    }
+                    Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+                    dialog.Title = "选择文件";
+                    dialog.Filter = "所有文件|*.*";
+                    if (dialog.ShowDialog() == true)
+                    {
+                        string strFilePath = dialog.FileName;
+                        string strSaveFilePath = string.Format("{0}\\{1}(Decrypt){2}", Path.GetDirectoryName(strFilePath), Path.GetFileNameWithoutExtension(strFilePath), Path.GetExtension(strFilePath));
+                        if (DESHelper.FileDESDecrypt(strFilePath, strSaveFilePath, TextDesKey, TextDesIV))
+                        {
+                            MessageBox.Show("Des文件解密成功!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Des文件解密失败!");
+                        }
+                    }
+                });
+            }
+        }
         #endregion
 
         #region RSA加密 Tab分页
