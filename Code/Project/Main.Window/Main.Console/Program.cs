e79ceead68ccc7cba8b63774e88ea0ac43c485b3
Sadness <qsbs623@163.com>
2018-07-07 21:28:16 +0800

    Add:Redis Helper

--- Code/Project/Main.Window/Main.Console/Program.cs
+++ Code/Project/Main.Window/Main.Console/Program.cs
@@ -9,6 +9,8 @@ using Utils.Helper.Encryption;
 using System.Drawing.Imaging;
 using Sadness.SQLiteDB.Connect;
 using ADO.Helper.DatabaseConversion;
+using Utils.Helper.Redis;
+using Newtonsoft.Json;
 
 namespace Main.Console
 {
@@ -16,20 +18,40 @@ namespace Main.Console
     {
         static void Main(string[] args)
         {
-            DataTable dt = new DataTable();
-            dt.Columns.Add("f1");
-            dt.Columns.Add("f2");
-            dt.Columns.Add("f3");
-            for (int i = 0; i < 25; i++)
+            RedisHelper redis = new RedisHelper();
+            //增
+            List<test> list0 = new List<test>();
+            list0.Add(new test { index = 0, bIsChoice = true, strUserNmae = "111", strPassword = "222" });
+            list0.Add(new test { index = 0, bIsChoice = true, strUserNmae = "333", strPassword = "444" });
+            list0.Add(new test { index = 0, bIsChoice = true, strUserNmae = "555", strPassword = "666" });
+            redis.StringSet("test", JsonConvert.SerializeObject(list0));
+            redis.StringSet("user:test", JsonConvert.SerializeObject(list0));
+            //改
+            List<test> list1 = new List<test>();
+            list1.Add(new test { index = 0, bIsChoice = true, strUserNmae = "aaa", strPassword = "bbb" });
+            list1.Add(new test { index = 0, bIsChoice = true, strUserNmae = "ccc", strPassword = "ddd" });
+            redis.StringSet("test", JsonConvert.SerializeObject(list1));
+            redis.StringSet("user:test", JsonConvert.SerializeObject(list1));
+            //查
+            if (redis.KeyExists("test"))
             {
-                DataRow dr = dt.NewRow();
-                dr["f1"] = i;
-                dr["f2"] = i;
-                dr["f3"] = i;
-                dt.Rows.Add(dr);
+                List<test> list2 = JsonConvert.DeserializeObject<List<test>>(redis.StringGet("test"));
             }
-            DataSet dsTest = DataProcessing.PagingQuery(dt, 99);
-            DataTable dtTest = DataProcessing.PagingQuery(dt, 2, 99);
+            if (redis.KeyExists("user:test"))
+            {
+                List<test> list3 = JsonConvert.DeserializeObject<List<test>>(redis.StringGet("user:test"));
+            }
+            //删
+            redis.KeyDelete("test");
+            redis.KeyDelete("user:test");
         }
+    }
+
+    public class test
+    {
+        public int index { get; set; }
+        public bool bIsChoice { get; set; }
+        public string strUserNmae { get; set; }
+        public string strPassword { get; set; }
     }
 }
