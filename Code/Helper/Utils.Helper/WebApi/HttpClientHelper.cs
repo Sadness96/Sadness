42a801578229e50fde1558363c1683e46aad7104
Sadness96 <qsbs623@163.com>
2018-08-27 17:28:58 +0800

    Add:WebApi Helper

---
+++ Code/Helper/Utils.Helper/WebApi/HttpClientHelper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.Helper.TXT;
+
+namespace Utils.Helper.WebApi
+{
+    /// <summary>
+    /// HttpClient帮助类
+    /// 创建日期:2018年8月27日
+    /// </summary>
+    public class HttpClientHelper
+    {
+        /// <summary>
+        /// 创建Get请求
+        /// </summary>
+        /// <param name="url">Api访问地址</param>
+        /// <param name="requestUrl">详细方法路径</param>
+        /// <param name="parameters">请求参数</param>
+        /// <returns>Api返回值</returns>
+        public static string CreateGetHttpClient(string url, string requestUrl, IDictionary<string, string> parameters)
+        {
+            try
+            {
+                StringBuilder builder = new StringBuilder();
+                builder.Append(url);
+                builder.Append(requestUrl);
+                if (parameters != null && parameters.Count >= 1)
+                {
+                    builder.Append("?");
+                    int i = 0;
+                    foreach (var item in parameters)
+                    {
+                        if (i > 0)
+                        {
+                            builder.Append("&");
+                        }
+                        builder.AppendFormat("{0}={1}", item.Key, item.Value);
+                        i++;
+                    }
+                }
+                HttpClient httpClient = new HttpClient();
+                httpClient.BaseAddress = new Uri(url);
+                var result = httpClient.GetAsync(builder.ToString()).Result;
+                return result.Content.ReadAsStringAsync().Result;
+            }
+            catch (Exception ex)
+            {
+                TXTHelper.Logs(ex.ToString());
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// 创建Post请求
+        /// </summary>
+        /// <param name="url">Api访问地址</param>
+        /// <param name="requestUrl">详细方法路径</param>
+        /// <param name="parameters">请求参数</param>
+        /// <returns>Api返回值</returns>
+        public static string CreatePostHttpClient(string url, string requestUrl, IDictionary<string, string> parameters)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                httpClient.BaseAddress = new Uri(url);
+                var result = httpClient.PostAsync(requestUrl, new FormUrlEncodedContent(parameters)).Result;
+                return result.Content.ReadAsStringAsync().Result;
+            }
+            catch (Exception ex)
+            {
+                TXTHelper.Logs(ex.ToString());
+                return "";
+            }
+        }
+    }
+}
---
+++ Code/Helper/Utils.Helper/WebApi/HttpWebRequestHelper.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.Helper.TXT;
+
+namespace Utils.Helper.WebApi
+{
+    /// <summary>
+    /// HttpWebRequest帮助类
+    /// 创建日期:2018年8月27日
+    /// </summary>
+    public class HttpWebRequestHelper
+    {
+        /// <summary>
+        /// 创建Get请求
+        /// </summary>
+        /// <param name="url">Api访问地址</param>
+        /// <param name="requestUrl">详细方法路径</param>
+        /// <param name="parameters">请求参数</param>
+        /// <returns>Api返回值</returns>
+        public static string CreateGetHttpWebRequest(string url, string requestUrl, IDictionary<string, string> parameters)
+        {
+            try
+            {
+                StringBuilder builder = new StringBuilder();
+                builder.Append(url);
+                builder.Append(requestUrl);
+                if (parameters != null && parameters.Count >= 1)
+                {
+                    builder.Append("?");
+                    int i = 0;
+                    foreach (var item in parameters)
+                    {
+                        if (i > 0)
+                        {
+                            builder.Append("&");
+                        }
+                        builder.AppendFormat("{0}={1}", item.Key, item.Value);
+                        i++;
+                    }
+                }
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(builder.ToString());
+                request.Method = "GET";
+                request.ContentType = "application/x-www-form-urlencoded";
+                StreamReader reader = new StreamReader(request.GetResponse().GetResponseStream(), Encoding.UTF8);
+                return reader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                TXTHelper.Logs(ex.ToString());
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// 创建Post请求
+        /// </summary>
+        /// <param name="url">Api访问地址</param>
+        /// <param name="requestUrl">详细方法路径</param>
+        /// <param name="parameters">请求参数</param>
+        /// <returns>Api返回值</returns>
+        public static string CreatePostHttpWebRequest(string url, string requestUrl, IDictionary<string, string> parameters)
+        {
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(url + requestUrl) as HttpWebRequest;
+                request.ProtocolVersion = HttpVersion.Version10;
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                //如果需要POST数据
+                if (!(parameters == null || parameters.Count == 0))
+                {
+                    StringBuilder buffer = new StringBuilder();
+                    int i = 0;
+                    foreach (string key in parameters.Keys)
+                    {
+                        if (i > 0)
+                        {
+                            buffer.AppendFormat("&{0}={1}", key, parameters[key]);
+                        }
+                        else
+                        {
+                            buffer.AppendFormat("{0}={1}", key, parameters[key]);
+                        }
+                        i++;
+                    }
+                    byte[] data = Encoding.GetEncoding("utf-8").GetBytes(buffer.ToString());
+                    using (Stream stream = request.GetRequestStream())
+                    {
+                        stream.Write(data, 0, data.Length);
+                    }
+                }
+                StreamReader reader = new StreamReader(request.GetResponse().GetResponseStream(), Encoding.UTF8);
+                return reader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                TXTHelper.Logs(ex.ToString());
+                return "";
+            }
+        }
+    }
+}
