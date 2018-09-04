e5e2925e12df9db4fdb20286c5601e57e09217b2
Sadness96 <qsbs623@163.com>
2018-09-04 15:24:01 +0800

    Modify:Bitmap To ImageSource

--- Code/Helper/Utils.Helper/Image/ImageHelper.cs
+++ Code/Helper/Utils.Helper/Image/ImageHelper.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using Utils.Helper.TXT;
 
 namespace Utils.Helper.Image
 {
@@ -36,8 +38,9 @@ namespace Utils.Helper.Image
                     return bitmapImage;
                 }
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                TXTHelper.Logs(ex.ToString());
                 return null;
             }
         }
@@ -56,8 +59,9 @@ namespace Utils.Helper.Image
                 ImageSource imageSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hBitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
                 return imageSource;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                TXTHelper.Logs(ex.ToString());
                 return null;
             }
         }
@@ -75,13 +79,41 @@ namespace Utils.Helper.Image
                 memoryStream = new MemoryStream(byteArray);
                 return new Bitmap((System.Drawing.Image)new Bitmap(memoryStream));
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                TXTHelper.Logs(ex.ToString());
                 return null;
             }
             finally
             {
                 memoryStream.Close();
+            }
+        }
+
+        /// <summary>
+        /// 注销对象方法API
+        /// </summary>
+        [DllImport("gdi32")]
+        private static extern int DeleteObject(IntPtr o);
+
+        /// <summary>
+        /// Bitmap 转 ImageSource
+        /// </summary>
+        /// <param name="bitmap">位图</param>
+        /// <returns>ImageSource</returns>
+        public static ImageSource BitmapToImageSource(Bitmap bitmap)
+        {
+            try
+            {
+                IntPtr hBitmap = bitmap.GetHbitmap();
+                BitmapSource bitmapSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hBitmap, IntPtr.Zero, Int32Rect.Empty, System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+                DeleteObject(hBitmap);
+                return bitmapSource;
+            }
+            catch (Exception ex)
+            {
+                TXTHelper.Logs(ex.ToString());
+                return null;
             }
         }
     }
