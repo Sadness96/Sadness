213c537b3ac564c19517476c9b3101ec5f85e322
Sadness96 <qsbs623@163.com>
2018-09-13 16:35:47 +0800

    Modify:HttpWebRequest Encoding Timeout

--- Code/Helper/Utils.Helper/WebApi/HttpWebRequestHelper.cs
+++ Code/Helper/Utils.Helper/WebApi/HttpWebRequestHelper.cs
@@ -57,6 +57,50 @@ namespace Utils.Helper.WebApi
         }
 
         /// <summary>
+        /// 创建Get请求
+        /// </summary>
+        /// <param name="url">Api访问地址</param>
+        /// <param name="requestUrl">详细方法路径</param>
+        /// <param name="parameters">请求参数</param>
+        /// <param name="encoding">字符编码</param>
+        /// <param name="timout ">请求超时前等待的毫秒数,默认值是 100,000 毫秒（100 秒）</param>
+        /// <returns>Api返回值</returns>
+        public static string CreateGetHttpWebRequest(string url, string requestUrl, IDictionary<string, string> parameters, Encoding encoding, int timout)
+        {
+            try
+            {
+                StringBuilder builder = new StringBuilder();
+                builder.Append(url);
+                builder.Append(requestUrl);
+                if (parameters != null && parameters.Count >= 1)
+                {
+                    builder.Append("?");
+                    int i = 0;
+                    foreach (var item in parameters)
+                    {
+                        if (i > 0)
+                        {
+                            builder.Append("&");
+                        }
+                        builder.AppendFormat("{0}={1}", item.Key, item.Value);
+                        i++;
+                    }
+                }
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(builder.ToString());
+                request.Method = "GET";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Timeout = timout;
+                StreamReader reader = new StreamReader(request.GetResponse().GetResponseStream(), encoding);
+                return reader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                TXTHelper.Logs(ex.ToString());
+                return "";
+            }
+        }
+
+        /// <summary>
         /// 创建Post请求
         /// </summary>
         /// <param name="url">Api访问地址</param>
@@ -89,6 +133,57 @@ namespace Utils.Helper.WebApi
                         i++;
                     }
                     byte[] data = Encoding.GetEncoding("utf-8").GetBytes(buffer.ToString());
+                    using (Stream stream = request.GetRequestStream())
+                    {
+                        stream.Write(data, 0, data.Length);
+                    }
+                }
+                StreamReader reader = new StreamReader(request.GetResponse().GetResponseStream(), Encoding.UTF8);
+                return reader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                TXTHelper.Logs(ex.ToString());
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// 创建Post请求
+        /// </summary>
+        /// <param name="url">Api访问地址</param>
+        /// <param name="requestUrl">详细方法路径</param>
+        /// <param name="parameters">请求参数</param>
+        /// <param name="encoding">字符编码</param>
+        /// <param name="timout ">请求超时前等待的毫秒数,默认值是 100,000 毫秒（100 秒）</param>
+        /// <returns>Api返回值</returns>
+        public static string CreatePostHttpWebRequest(string url, string requestUrl, IDictionary<string, string> parameters, Encoding encoding, int timout)
+        {
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(url + requestUrl) as HttpWebRequest;
+                request.ProtocolVersion = HttpVersion.Version10;
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Timeout = timout;
+                //如果需要POST数据
+                if (!(parameters == null || parameters.Count == 0))
+                {
+                    StringBuilder buffer = new StringBuilder();
+                    int i = 0;
+                    foreach (string key in parameters.Keys)
+                    {
+                        if (i > 0)
+                        {
+                            buffer.AppendFormat("&{0}={1}", key, parameters[key]);
+                        }
+                        else
+                        {
+                            buffer.AppendFormat("{0}={1}", key, parameters[key]);
+                        }
+                        i++;
+                    }
+                    byte[] data = encoding.GetBytes(buffer.ToString());
                     using (Stream stream = request.GetRequestStream())
                     {
                         stream.Write(data, 0, data.Length);
