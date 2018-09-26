f572d0d53e9c9ea6e3342fb60e98636c3ec7107c
Sadness96 <qsbs623@163.com>
2018-09-26 15:56:05 +0800

    Add:GZIP Helper

---
+++ Code/Helper/FileIO.Helper/ZIP/GZIPHelper.cs
@@ -0,0 +1,124 @@
+﻿using FileIO.Helper.TXT;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO.Helper.ZIP
+{
+    /// <summary>
+    /// GZIP压缩帮助类
+    /// 创建日期:2018年9月26日
+    /// </summary>
+    public class GZIPHelper
+    {
+        /// <summary>
+        /// 压缩GZIP数据
+        /// </summary>
+        /// <param name="bytesSourceData">源数据</param>
+        /// <returns>压缩数据</returns>
+        public static byte[] CompressionGZIP(byte[] bytesSourceData)
+        {
+            try
+            {
+                MemoryStream memoryStream = new MemoryStream();
+                GZipStream compressedzipStream = new GZipStream(memoryStream, CompressionMode.Compress, true);
+                compressedzipStream.Write(bytesSourceData, 0, bytesSourceData.Length);
+                compressedzipStream.Close();
+                return memoryStream.ToArray();
+            }
+            catch (Exception ex)
+            {
+                TXTHelper.Logs(ex.ToString());
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 解压缩GZIP数据
+        /// </summary>
+        /// <param name="bytesSourceData">源数据</param>
+        /// <returns>解压缩数据</returns>
+        public static byte[] DeCompressionGZIP(byte[] bytesSourceData)
+        {
+            try
+            {
+                MemoryStream memoryStream = new MemoryStream(bytesSourceData);
+                GZipStream compressedzipStream = new GZipStream(memoryStream, CompressionMode.Decompress);
+                MemoryStream outBuffer = new MemoryStream();
+                byte[] block = new byte[1024];
+                while (true)
+                {
+                    int bytesRead = compressedzipStream.Read(block, 0, block.Length);
+                    if (bytesRead <= 0)
+                        break;
+                    else
+                        outBuffer.Write(block, 0, bytesRead);
+                }
+                compressedzipStream.Close();
+                return outBuffer.ToArray();
+            }
+            catch (Exception ex)
+            {
+                TXTHelper.Logs(ex.ToString());
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 压缩String类型GZIP数据
+        /// </summary>
+        /// <param name="strSourceData">源数据</param>
+        /// <returns>压缩数据(Base64)</returns>
+        public static string CompressionStringGZIP(string strSourceData)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(strSourceData))
+                {
+                    byte[] rawData = Encoding.UTF8.GetBytes(strSourceData);
+                    byte[] zippedData = CompressionGZIP(rawData);
+                    return Convert.ToBase64String(zippedData);
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+            catch (Exception ex)
+            {
+                TXTHelper.Logs(ex.ToString());
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// 解压缩String类型GZIP数据
+        /// </summary>
+        /// <param name="strSourceData">源数据(Base64)</param>
+        /// <returns>解压缩数据</returns>
+        public static string DeCompressionStringGZIP(string strSourceData)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(strSourceData))
+                {
+                    byte[] zippedData = Convert.FromBase64String(strSourceData.ToString());
+                    return Encoding.UTF8.GetString(DeCompressionGZIP(zippedData));
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+            catch (Exception ex)
+            {
+                TXTHelper.Logs(ex.ToString());
+                return string.Empty;
+            }
+        }
+    }
+}
--- Code/Project/Main.Window/Main.Console/Program.cs
+++ Code/Project/Main.Window/Main.Console/Program.cs
@@ -9,9 +9,11 @@ using Utils.Helper.Encryption;
 using System.Drawing.Imaging;
 using Sadness.SQLiteDB.Connect;
 using ADO.Helper.DatabaseConversion;
+using FileIO.Helper.ZIP;
 using Utils.Helper.Redis;
 using Utils.Helper.CheckCorrectness;
 using Newtonsoft.Json;
+using FileIO.Helper.TXT;
 
 namespace Main.Console
 {
@@ -19,40 +21,49 @@ namespace Main.Console
     {
         static void Main(string[] args)
         {
-            bool b1 = CheckCorrectnessHelper.CheckInternetProtocol("36.102.228.108");
-            bool b2 = CheckCorrectnessHelper.CheckInternetProtocol("192.168.2.123");
-            bool b3 = CheckCorrectnessHelper.CheckInternetProtocol("255.255.255.255");
-            bool b4 = CheckCorrectnessHelper.CheckInternetProtocol("300.255.255.255");
-            bool b5 = CheckCorrectnessHelper.CheckInternetProtocol("256.255.255.0");
-            bool b6 = CheckCorrectnessHelper.CheckInternetProtocol("0.0.134.255");
+            #region GZIP压缩测试方法
+            //string strJsonData = TXTHelper.GetFileString(@"\\192.168.2.64\共享文件\test.txt");
+            //string Compression = GZIPHelper.CompressionStringGZIP(strJsonData);
+            //string DeCompression = GZIPHelper.DeCompressionStringGZIP(Compression);
+            #endregion
 
+            #region IP地址效验测试方法
+            //bool b1 = CheckCorrectnessHelper.CheckInternetProtocol("36.102.228.108");
+            //bool b2 = CheckCorrectnessHelper.CheckInternetProtocol("192.168.2.123");
+            //bool b3 = CheckCorrectnessHelper.CheckInternetProtocol("255.255.255.255");
+            //bool b4 = CheckCorrectnessHelper.CheckInternetProtocol("300.255.255.255");
+            //bool b5 = CheckCorrectnessHelper.CheckInternetProtocol("256.255.255.0");
+            //bool b6 = CheckCorrectnessHelper.CheckInternetProtocol("0.0.134.255");
+            #endregion
 
-            RedisHelper redis = new RedisHelper();
-            //增
-            List<test> list0 = new List<test>();
-            list0.Add(new test { index = 0, bIsChoice = true, strUserNmae = "111", strPassword = "222" });
-            list0.Add(new test { index = 0, bIsChoice = true, strUserNmae = "333", strPassword = "444" });
-            list0.Add(new test { index = 0, bIsChoice = true, strUserNmae = "555", strPassword = "666" });
-            redis.StringSet("test", JsonConvert.SerializeObject(list0));
-            redis.StringSet("user:test", JsonConvert.SerializeObject(list0));
-            //改
-            List<test> list1 = new List<test>();
-            list1.Add(new test { index = 0, bIsChoice = true, strUserNmae = "aaa", strPassword = "bbb" });
-            list1.Add(new test { index = 0, bIsChoice = true, strUserNmae = "ccc", strPassword = "ddd" });
-            redis.StringSet("test", JsonConvert.SerializeObject(list1));
-            redis.StringSet("user:test", JsonConvert.SerializeObject(list1));
-            //查
-            if (redis.KeyExists("test"))
-            {
-                List<test> list2 = JsonConvert.DeserializeObject<List<test>>(redis.StringGet("test"));
-            }
-            if (redis.KeyExists("user:test"))
-            {
-                List<test> list3 = JsonConvert.DeserializeObject<List<test>>(redis.StringGet("user:test"));
-            }
-            //删
-            redis.KeyDelete("test");
-            redis.KeyDelete("user:test");
+            #region Redis测试方法
+            //RedisHelper redis = new RedisHelper();
+            ////增
+            //List<test> list0 = new List<test>();
+            //list0.Add(new test { index = 0, bIsChoice = true, strUserNmae = "111", strPassword = "222" });
+            //list0.Add(new test { index = 0, bIsChoice = true, strUserNmae = "333", strPassword = "444" });
+            //list0.Add(new test { index = 0, bIsChoice = true, strUserNmae = "555", strPassword = "666" });
+            //redis.StringSet("test", JsonConvert.SerializeObject(list0));
+            //redis.StringSet("user:test", JsonConvert.SerializeObject(list0));
+            ////改
+            //List<test> list1 = new List<test>();
+            //list1.Add(new test { index = 0, bIsChoice = true, strUserNmae = "aaa", strPassword = "bbb" });
+            //list1.Add(new test { index = 0, bIsChoice = true, strUserNmae = "ccc", strPassword = "ddd" });
+            //redis.StringSet("test", JsonConvert.SerializeObject(list1));
+            //redis.StringSet("user:test", JsonConvert.SerializeObject(list1));
+            ////查
+            //if (redis.KeyExists("test"))
+            //{
+            //    List<test> list2 = JsonConvert.DeserializeObject<List<test>>(redis.StringGet("test"));
+            //}
+            //if (redis.KeyExists("user:test"))
+            //{
+            //    List<test> list3 = JsonConvert.DeserializeObject<List<test>>(redis.StringGet("user:test"));
+            //}
+            ////删
+            //redis.KeyDelete("test");
+            //redis.KeyDelete("user:test");
+            #endregion
         }
     }
 
