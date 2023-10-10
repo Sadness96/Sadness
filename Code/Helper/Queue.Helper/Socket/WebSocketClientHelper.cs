af02bc856b7d2801803769bfe9444537ef925f45
Sadness96 <qsbs623@163.com>
2023-10-10 15:47:51 +0800

    WebSocket帮助类

---
+++ Code/Helper/Queue.Helper/Socket/WebSocketClientHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Queue.Helper.Socket
+{
+    /// <summary>
+    /// WebSocket 客户端帮助类
+    /// </summary>
+    public class WebSocketClientHelper
+    {
+        private ClientWebSocket clientWebSocket;
+        private CancellationTokenSource cancellationTokenSource;
+
+        /// <summary>
+        /// 收到数据回调
+        /// </summary>
+        public event Action<string> OnDataReceived;
+
+        /// <summary>
+        /// 连接服务端
+        /// </summary>
+        /// <param name="url">ws://localhost:8080/</param>
+        /// <returns></returns>
+        public async Task Connect(string url)
+        {
+            clientWebSocket = new ClientWebSocket();
+            cancellationTokenSource = new CancellationTokenSource();
+
+            await clientWebSocket.ConnectAsync(new Uri(url), cancellationTokenSource.Token);
+
+            await Task.Factory.StartNew(async () =>
+            {
+                while (clientWebSocket.State == WebSocketState.Open)
+                {
+                    byte[] buffer = new byte[1024];
+                    WebSocketReceiveResult result = await clientWebSocket.ReceiveAsync(new ArraySegment<byte>(buffer), cancellationTokenSource.Token);
+
+                    if (result.MessageType == WebSocketMessageType.Text)
+                    {
+                        string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                        OnDataReceived?.Invoke(message);
+                    }
+                    else if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        await clientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", cancellationTokenSource.Token);
+                    }
+                }
+            }, cancellationTokenSource.Token);
+        }
+
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        /// <param name="message">消息</param>
+        /// <returns></returns>
+        public async Task Send(string message)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            await clientWebSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, cancellationTokenSource.Token);
+        }
+
+        /// <summary>
+        /// 关闭
+        /// </summary>
+        /// <returns></returns>
+        public async Task Close()
+        {
+            cancellationTokenSource.Cancel();
+            await clientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", cancellationTokenSource.Token);
+        }
+    }
+}
---
+++ Code/Helper/Queue.Helper/Socket/WebSocketServerHelper.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Queue.Helper.Socket
+{
+    /// <summary>
+    /// WebSocket 服务端帮助类
+    /// </summary>
+    public class WebSocketServer
+    {
+        private HttpListener listener;
+        private CancellationTokenSource cancellationTokenSource;
+        private List<WebSocket> webSockets;
+
+        /// <summary>
+        /// 收到数据回调
+        /// </summary>
+        public event Action<WebSocket, string> OnDataReceived;
+
+        /// <summary>
+        /// 启动
+        /// </summary>
+        /// <param name="url">http://localhost:8080/</param>
+        /// <returns></returns>
+        public async Task Start(string url)
+        {
+            Console.WriteLine($"WebSocket Server listen:{url}");
+
+            listener = new HttpListener();
+            listener.Prefixes.Add(url);
+            listener.Start();
+
+            cancellationTokenSource = new CancellationTokenSource();
+            webSockets = new List<WebSocket>();
+
+            while (!cancellationTokenSource.IsCancellationRequested)
+            {
+                HttpListenerContext context = await listener.GetContextAsync();
+
+                if (context.Request.IsWebSocketRequest)
+                {
+                    ProcessWebSocketRequest(context);
+                }
+                else
+                {
+                    context.Response.StatusCode = 400;
+                    context.Response.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 停止
+        /// </summary>
+        public void Stop()
+        {
+            cancellationTokenSource.Cancel();
+            listener.Stop();
+        }
+
+        /// <summary>
+        /// 处理 WebSocket 请求
+        /// </summary>
+        /// <param name="context"></param>
+        private async void ProcessWebSocketRequest(HttpListenerContext context)
+        {
+            WebSocketContext webSocketContext = await context.AcceptWebSocketAsync(subProtocol: null);
+            WebSocket webSocket = webSocketContext.WebSocket;
+            webSockets.Add(webSocket);
+            Console.WriteLine($"Add client connection:{context.Request.RemoteEndPoint}");
+
+            try
+            {
+                while (webSocket.State == WebSocketState.Open)
+                {
+                    byte[] buffer = new byte[1024];
+                    WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), cancellationTokenSource.Token);
+
+                    if (result.MessageType == WebSocketMessageType.Text)
+                    {
+                        string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                        OnDataReceived.Invoke(webSocket, message);
+                    }
+                    else if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        webSockets.Remove(webSocket);
+                        Console.WriteLine($"Break client connection:{context.Request.RemoteEndPoint}");
+                        await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", cancellationTokenSource.Token);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                webSockets.Remove(webSocket);
+                Console.WriteLine($"Break client connection:{context.Request.RemoteEndPoint}");
+            }
+        }
+
+        /// <summary>
+        /// 发送消息给所有客户端
+        /// </summary>
+        /// <param name="data">消息</param>
+        /// <returns></returns>
+        public async Task SendToAllAsync(string data)
+        {
+            List<Task> tasks = new List<Task>();
+
+            if (webSockets != null && webSockets.Count >= 1)
+            {
+                foreach (WebSocket webSocket in webSockets)
+                {
+                    tasks.Add(webSocket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(data)), WebSocketMessageType.Text, true, cancellationTokenSource.Token));
+                }
+
+                await Task.WhenAll(tasks);
+            }
+        }
+
+        /// <summary>
+        /// 发送消息给指定客户端
+        /// </summary>
+        /// <param name="client">客户端</param>
+        /// <param name="data">消息</param>
+        /// <returns></returns>
+        public async Task SendToClientAsync(WebSocket client, string data)
+        {
+            await client.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(data)), WebSocketMessageType.Text, true, cancellationTokenSource.Token);
+        }
+    }
+}
